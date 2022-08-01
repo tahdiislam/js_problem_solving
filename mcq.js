f44cb8b8d42862bdd253ad5512dbf19ac14d7794
tahdi-islam <tshdi60@gmail.com>
2022-08-02 03:54:27 +0600

    practice some problem

---
+++ mcq.js
@@ -0,0 +1,38 @@
+var i = 0;
+for (i=0; i<5; i++){};
+console.log(i);
+
+// 2nd 
+var marks = [13, 15, 14, 20, 18];
+for(var i=0; i<marks.length; i++){
+    if(marks[i] >= 15){
+        continue;
+    }
+    console.log(marks[i]);
+}
+
+// 3rd
+
+for (var i=0; i<10; i++){
+    console.log(i);
+}
+
+// 4th
+
+var name01 = "false";
+console.log(typeof name01);
+
+// 5th  */
+
+ var i = 5;
+for (; i<5; i++) {
+    console.log(i);
+}
+
+// 6th
+
+
+var i = 5;
+for (i < 5; i++) {
+    console.log(i);
+}
