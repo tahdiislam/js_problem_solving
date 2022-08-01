ab39dbd2b2cb3dfcacfde77902f105617bb97146
tahdi-islam <tshdi60@gmail.com>
2022-08-02 01:16:15 +0600

    solve problem in mdl 17

---
+++ while_loop.js
@@ -0,0 +1,15 @@
+var roastGiven = 0;
+while(roastGiven < 8) {
+    // console.log('Roast den please !!')
+    // console.log(roastGiven);
+    roastGiven++;
+}
+
+// print 1 to 10
+
+var number = 0;
+while (number < 11){
+    console.log(number);
+    number++;
+}
+
