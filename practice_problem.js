72ae36019c121cc855980f855c6c2a99e41b57e6
tahdi-islam <tshdi60@gmail.com>
2022-08-03 13:32:46 +0600

    solve prb_3

--- practice_problem.js
+++ practice_problem.js
@@ -4,4 +4,6 @@
     2. The web browser loads a web page, parses the HTML, creates what is known as a Document Object Model (DOM) from the contents. The DOM presents a live view of the web page to your Javascript code. 
     The brower will then grab everything linked to the HTML, like images and CSS files.
 
+    3. Variable means anything that can vary. in JavaScript, a variable stores the data value that can be changed later on. Use the reserved keyword var to declare a variable in JavaScript.
+
 */
