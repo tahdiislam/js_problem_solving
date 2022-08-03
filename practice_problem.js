d72b9474ae107d0a26b9f34ff8ba65baf62ef312
tahdi-islam <tshdi60@gmail.com>
2022-08-03 13:27:46 +0600

    create mdl-18.5 problem solving file

---
+++ practice_problem.js
@@ -0,0 +1,4 @@
+/* 
+    1. Javascript is scripting or programming language that's used for web development, in web applications, for game development, and lots more. It allows you to implement dynamic features on web pages that cannot be done with HTML and CSS
+
+*/
