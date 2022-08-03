61f5424231ded03e257aba51c2587eadaa606db4
tahdi-islam <tshdi60@gmail.com>
2022-08-03 13:29:39 +0600

    solve prb_2

--- practice_problem.js
+++ practice_problem.js
@@ -1,4 +1,7 @@
 /* 
     1. Javascript is scripting or programming language that's used for web development, in web applications, for game development, and lots more. It allows you to implement dynamic features on web pages that cannot be done with HTML and CSS
 
+    2. The web browser loads a web page, parses the HTML, creates what is known as a Document Object Model (DOM) from the contents. The DOM presents a live view of the web page to your Javascript code. 
+    The brower will then grab everything linked to the HTML, like images and CSS files.
+
 */
